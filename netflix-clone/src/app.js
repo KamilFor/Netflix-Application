5857b816f052461478e7bde00ea2230baf0642cd
Kamil Fornalski <kester.kamil@gmail.com>
2021-01-17 23:43:59 +0100

    52.27

--- netflix-clone/src/app.js
+++ netflix-clone/src/app.js
@@ -6,10 +6,15 @@ export default function App() {
   return (
     <Jumbotron.Container>
       {jumboData.map((item) => {
-        <Jumbotron key={item.id} direction={item.direction}>
-          <p>Hello</p>
-          aa
-        </Jumbotron>;
+        return (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt} />
+          </Jumbotron>
+        );
       })}
     </Jumbotron.Container>
   );
